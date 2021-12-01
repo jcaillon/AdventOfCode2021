e0d6a76dc8eaa599944aa22c7116174cc7b090b4
Julien Caillon <julien.caillon@gmail.com>
2021-12-01 20:25:28 +0100

    day1-part2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace AdventOfCode
     {
         static void Main(string[] args) {
             var depths = File.ReadAllLines("input").Select(s => int.Parse(s)).ToList();
-            var depthDifference = depths.Select((depth, index) => index > 0 ? depth - depths[index - 1] : 0);
+            const int slidingWindow = 3;
+            var depthSliding = depths
+                .Select((depth, index) => depths.Skip(index).Take(slidingWindow).Sum())
+                .SkipLast(slidingWindow - 1)
+                .ToArray();
+            var depthDifference = depthSliding.Select((depth, index) => index > 0 ? depth - depthSliding[index - 1] : 0);
             Console.WriteLine(depthDifference.Count(diff => diff > 0));
         }
     }
---
+++ README.md
@@ -0,0 +1,9 @@
+# AdventOfCode2021
+
+https://adventofcode.com/
+
+1 commit = 1 solution
+
+```bash
+dotnet run
+```
