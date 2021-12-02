a0053274b44f2b7567b78c9b096d7b550276f8bf
Julien Caillon <julien.caillon@gmail.com>
2021-12-02 12:58:37 +0100

    ✨ added gitpod

--- Program.cs
+++ Program.cs
@@ -1,57 +1,44 @@
-using System;
-using DotUtilities;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace AdventOfCode
-{
-    class Program
-    {
-        static void Main(string[] args) {
-            var subMarine = new SubMarine();
-            subMarine.Move(File.ReadAllLines("input").Select(s => new MovementInstruction(s)));
-            Console.WriteLine(subMarine.Distance * subMarine.Depth);
-            Console.ReadKey();
-        }
-    }
+var subMarine = new SubMarine();
+subMarine.Move(File.ReadAllLines("input").Select(s => new MovementInstruction(s)));
+Console.WriteLine(subMarine.Distance * subMarine.Depth);
 
-    class SubMarine {
-        public int Depth { get; set; } = 0;
-        public int Distance { get; set; } = 0;
-        public int Aim { get; set; } = 0;
+class SubMarine {
+    public int Depth { get; set; } = 0;
+    public int Distance { get; set; } = 0;
+    public int Aim { get; set; } = 0;
 
-        public void Move(IEnumerable<MovementInstruction> instructions) {
-            foreach (var instruction in instructions) {
-                switch (instruction.Order) {
-                    case OrderType.Forward:
-                        Distance += instruction.Value;
-                        Depth += Aim * instruction.Value;
-                        break;
-                    case OrderType.Up:
-                        Aim -= instruction.Value;
-                        break;
-                    case OrderType.Down:
-                        Aim += instruction.Value;
-                        break;
-                }
+    public void Move(IEnumerable<MovementInstruction> instructions) {
+        foreach (var instruction in instructions) {
+            switch (instruction.Order) {
+                case OrderType.Forward:
+                    Distance += instruction.Value;
+                    Depth += Aim * instruction.Value;
+                    break;
+                case OrderType.Up:
+                    Aim -= instruction.Value;
+                    break;
+                case OrderType.Down:
+                    Aim += instruction.Value;
+                    break;
             }
         }
     }
+}
 
-    class MovementInstruction {
-        public OrderType Order { get; set; }
-        public int Value { get; set; }
-        public MovementInstruction(string instruction) {
-            var splittedInstruction = instruction.Split(' ');
-            Order = (OrderType) Enum.Parse(typeof(OrderType), splittedInstruction[0], true);
-            Value = int.Parse(splittedInstruction[1]);
-        }
-    }
-
-    enum OrderType {
-        Forward,
-        Down,
-        Up
+class MovementInstruction {
+    public OrderType Order { get; set; }
+    public int Value { get; set; }
+    public MovementInstruction(string instruction) {
+        var splittedInstruction = instruction.Split(' ');
+        Order = (OrderType) Enum.Parse(typeof(OrderType), splittedInstruction[0], true);
+        Value = int.Parse(splittedInstruction[1]);
     }
 }
+
+enum OrderType {
+    Forward,
+    Down,
+    Up
+}
+
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ https://adventofcode.com/
 
 1 commit = 1 solution
 
+[![gitpod](https://img.shields.io/badge/Open%20in-Gitpod-C8597A.svg)](https://gitpod.io/#https://github.com/jcaillon/AdventOfCode2021)
+
 ```bash
 dotnet run
 ```
