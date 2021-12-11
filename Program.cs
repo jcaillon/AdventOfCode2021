62f4d21d447645fb537882067fafae8ea73a6994
Julien Caillon <julien.caillon@gmail.com>
2021-12-11 13:25:12 +0100

    day11-part1

--- Program.cs
+++ Program.cs
@@ -1,100 +1,80 @@
 using System.Diagnostics;
 
-//Console.WriteLine(Puzzle.Solve("input-test"));
-Console.WriteLine(Puzzle.Solve("input"));
+Console.WriteLine(Puzzle.Solve("input-test", 10));
+Console.WriteLine(Puzzle.Solve("input-test", 100));
+Console.WriteLine(Puzzle.Solve("input", 100));
 
 static class Puzzle {
-    public static string Solve(string inputFilePath) {
+    public static string Solve(string inputFilePath, int nbSteps) {
         var inputList = File.ReadAllLines(inputFilePath);
-
-        var nav = new NavigationSystem(inputList);
-        var incomplete = nav.ParsedLines.Where(p => !string.IsNullOrEmpty(p.MissingChars)).OrderBy(p => p.MissingCharScore).ToList();
-        foreach (var inc in incomplete) {
-            Console.WriteLine($"{inc.MissingChars} -> {inc.MissingCharScore}");
-        }
-        return $"Middle score is {incomplete[incomplete.Count / 2].MissingCharScore}";
-    }
-}
-
-class NavigationSystem {
-    public List<ParsedLine> ParsedLines { get; private set; }
-    public NavigationSystem(IEnumerable<string> navigationCode) {
-        ParsedLines = navigationCode.Select(l => new ParsedLine(l)).ToList();
-    }
-}
-
-class ParsedLine {
-    private char [] legalStartingChar = new char[] { '(', '[', '{', '<' };
-    private char[] legalEndingChar = new char[] { ')', ']', '}', '>' };
-    private int[] charScore = new int[] { 3, 57, 1197, 25137 };
-    private ulong[] missingCharScore = new ulong[] { 1, 2, 3, 4 };
-    public string RawLine { get; }
-    public List<string> Chunks { get; }
-    public int FirstIllegalCharPosition { get; } = -1;
-    public bool IsCorrupted => FirstIllegalCharPosition > -1;
-    public bool IsIncomplete => FirstIllegalCharPosition == -1 && !string.IsNullOrEmpty(MissingChars);
-    public string? MissingChars { get; }
-    public int Score {
-        get {
-            var indexOfFirstIllegalChar = legalEndingChar.FindIndex(FirstIllegalChar);
-            return indexOfFirstIllegalChar > -1 ? charScore[indexOfFirstIllegalChar] : 0;
-        }
-    }
-    public ulong MissingCharScore {
-        get {
-            var total = (ulong) 0;
-            if (!string.IsNullOrEmpty(MissingChars)) {
-                foreach (var c in MissingChars) {
-                    var idx = legalEndingChar.FindIndex(c);
-                    var f = missingCharScore[legalEndingChar.FindIndex(c)];
-                    total = total * 5 + missingCharScore[legalEndingChar.FindIndex(c)];
-                }
-            }
-            return total;
-        }
-    }
-    public char FirstIllegalChar => FirstIllegalCharPosition > -1 ? RawLine[FirstIllegalCharPosition] : (char) 0;
-    public ParsedLine(string rawLine) {
-        RawLine = rawLine;
-        Chunks = new List<string>();
-        MissingChars = null;
-        var openedCharIndexStack = new Stack<int>();
-        var chunkStartPos = 0;
-        for (var currentPos = 0; currentPos < RawLine.Length; currentPos++) {
-            var currentChar = RawLine[currentPos];
-            var foundOpeningCharIndex = legalStartingChar.FindIndex(currentChar);
-            if (foundOpeningCharIndex > -1) {
-                openedCharIndexStack.Push(foundOpeningCharIndex);
-            } else {
-                var foundClosingCharIndex = legalEndingChar.FindIndex(currentChar);
-                if (foundClosingCharIndex > -1) {
-                    var lastOpeningCharIndex = openedCharIndexStack.Pop();
-                    if (lastOpeningCharIndex != foundClosingCharIndex) {
-                        FirstIllegalCharPosition = currentPos;
-                        break;
-                    } else if (openedCharIndexStack.Count == 0) {
-                        Chunks.Add(rawLine.Substring(chunkStartPos, currentPos - chunkStartPos + 1));
-                        chunkStartPos = currentPos + 1;
-                    }
-                } else {
-                    // wrong char
-                    FirstIllegalCharPosition = currentPos;
-                    break;
+        var cavern = new Cavern(inputList.SelectMany(c => c).Select(c => byte.Parse($"{c}")).ToList(), inputList[0].Length, inputList.Length);
+        var totalFlashes = 0;
+        for (int i = 0; i < nbSteps; i++) {
+            cavern.NextStep();
+            totalFlashes += cavern.TotalFlashNumber;
+            if (nbSteps < 5) {
+                Console.WriteLine($"Step {cavern.Step}");
+                for (int j = 0; j < cavern.MapHeight; j++) {
+                    Console.WriteLine(String.Concat(cavern.OctopusesEnergyLevel.Skip(j * cavern.MapWidth).Take(cavern.MapWidth)));
                 }
             }
         }
-        if (FirstIllegalCharPosition == -1 && openedCharIndexStack.Count > 0) {
-            MissingChars = string.Concat(openedCharIndexStack.Select(i => legalEndingChar[i]));
-        }
+        return $"After {nbSteps} steps, there has been a total of {totalFlashes} flashes.";
     }
 }
 
-public static class ParsedLineExtensions {
-    public static int FindIndex(this char[] chars, char charToFind) {
-        for (int i = 0; i < chars.Length; i++) {
-            if (chars[i] == charToFind) {
-                return i;
+class Cavern {
+    public List<byte> OctopusesEnergyLevel { get; private set; }
+
+    public int MapWidth { get; private set; }
+    public int MapHeight { get; private set; }
+    public int Step { get; private set; }
+    public int TotalFlashNumber => OctopusesEnergyLevel.Count(energy => energy == 0);
+
+    public Cavern(List<byte> octopusesEnergyLevel, int mapWidth, int mapHeight) {
+        OctopusesEnergyLevel = octopusesEnergyLevel;
+        MapWidth = mapWidth;
+        MapHeight = mapHeight;
+        Step = 0;
+        Debug.Assert(MapHeight * MapWidth == OctopusesEnergyLevel.Count);
+    }
+
+    public void NextStep() {
+        Step++;
+        var indexesFlashing = new Queue<int>();
+        for (int i = 0; i < OctopusesEnergyLevel.Count; i++) {
+            if (++OctopusesEnergyLevel[i] > 9) {
+                indexesFlashing.Enqueue(i);
             }
+        }
+        Debug.Assert(OctopusesEnergyLevel.All(e => e > 0));
+        var adjacentDeltas = new int[] { -MapWidth - 1, -MapWidth, -MapWidth + 1, -1, 1, MapWidth - 1, MapWidth, MapWidth + 1 };
+        var adjacentDeltasX = new int[] { -1, 0, +1, -1, +1, -1, 0, +1 };
+        var adjacentDeltasY = new int[] { -1, -1, -1, 0, 0, +1, +1, +1 };
+        while(indexesFlashing.Count > 0) {
+            var currentIndex = indexesFlashing.Dequeue();
+            var currentX = currentIndex % MapWidth;
+            var currentY = currentIndex / MapWidth;
+            var adjacentDeltaIndexesAllowed = new List<int>();
+            for (int i = 0; i < adjacentDeltas.Length; i++) {
+                if (currentX + adjacentDeltasX[i] >= 0 && currentX + adjacentDeltasX[i] < MapWidth
+                    && currentY + adjacentDeltasY[i] >= 0 && currentY + adjacentDeltasY[i] < MapHeight) {
+                    adjacentDeltaIndexesAllowed.Add(i);
+                }
+            }
+            foreach (var adjacentDeltaIndexAllowed in adjacentDeltaIndexesAllowed) {
+                var adjacentIndex = currentIndex + adjacentDeltas[adjacentDeltaIndexAllowed];
+                if (IncreaseEnergyLevel(adjacentIndex) > 9) {
+                    indexesFlashing.Enqueue(adjacentIndex);
+                }
+            }
+        }
+        OctopusesEnergyLevel = OctopusesEnergyLevel.Select(energy => energy > 9 ? (byte) 0 : energy).ToList();
+    }
+
+    private int IncreaseEnergyLevel(int index) {
+        if (index >= 0 && index < OctopusesEnergyLevel.Count && OctopusesEnergyLevel[index] <= 9) {
+            return ++OctopusesEnergyLevel[index];
         }
         return -1;
     }
