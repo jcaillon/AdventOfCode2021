0f3325b25da614f9145449e14bc06c3b6ca2295a
Julien Caillon <julien.caillon@gmail.com>
2021-12-13 16:11:55 +0100

    day13-part1

--- Program.cs
+++ Program.cs
@@ -1,109 +1,84 @@
-Console.WriteLine(Puzzle.Solve("input-test"));
-Console.WriteLine(Puzzle.Solve("input"));
+using System.Diagnostics;
+using System.Drawing;
+
+Console.WriteLine(Puzzle.Solve("input-test", true));
+Console.WriteLine(Puzzle.Solve("input", false));
 
 static class Puzzle {
-    public static string Solve(string inputFilePath) {
+    public static string Solve(string inputFilePath, bool printMap) {
         var inputList = File.ReadAllLines(inputFilePath);
-        var cavern = new Cavern(inputList.Select(s => new Connexion(new Cave(s.Split('-')[0]), new Cave(s.Split('-')[1]))).ToList());
+        var dotLocations = inputList.Where(s => s.Contains(',')).Select(s => new Point(int.Parse(s.Split(',')[0]), int.Parse(s.Split(',')[1]))).ToList();
+        var origami = new Origami(dotLocations, dotLocations.Select(p => p.X).Max() + 1, dotLocations.Select(p => p.Y).Max() + 1);
 
-        if (cavern.Paths.Count < 50) {
-            foreach (var path in cavern.Paths) {
-                Console.WriteLine(string.Join(',', path.Caves.Select(c => c.Name)));
+        if (printMap)
+            origami.PrintToConsole();
+
+        var foldInstructions = inputList.Where(s => s.Contains('=')).Select(s => s.Split(' ')[2]).Take(1).ToList();
+        foreach (var foldInstr in foldInstructions.Select(s => s.Split('='))) {
+            if (foldInstr[0] == "y") {
+                origami.FoldHorizontally(int.Parse(foldInstr[1]));
+            } else {
+                origami.FoldVertically(int.Parse(foldInstr[1]));
+            }
+            if (printMap)
+                origami.PrintToConsole();
+        }
+
+        return $"There are {origami.DotsMap.Count(dot => dot)} dots visible.";
+    }
+}
+
+class Origami {
+    public List<bool> DotsMap { get; private set; }
+    public int MapWidth { get; private set; }
+    public int MapHeight { get; private set; }
+
+    public Origami(List<Point> dotLocations, int mapWidth, int mapHeight) {
+        MapWidth = mapWidth;
+        MapHeight = mapHeight;
+        DotsMap = Enumerable.Repeat(false, mapWidth * mapHeight).ToList();
+        dotLocations.ForEach(p => DotsMap[p.X + p.Y * MapWidth] = true);
+    }
+
+    public void FoldHorizontally(int line) {
+        var nbLinesToFold = MapHeight - line - 1;
+        for (int foldedLine = 1; foldedLine <= nbLinesToFold; foldedLine++) {
+            for (int i = 0; i < MapWidth; i++) {
+                var currentIndex = i + (line + foldedLine) * MapWidth;
+                var newIndex = i + (line - foldedLine) * MapWidth;
+                DotsMap[newIndex] = DotsMap[newIndex] || DotsMap[currentIndex];
             }
         }
-
-        return $"There are {cavern.Paths.Count} paths through this cave system.";
-    }
-}
-
-class Cavern {
-    public List<Connexion> Connexions { get; private set; }
-    public List<Path> Paths { get; private set; } = new List<Path>();
-
-    public Cavern(List<Connexion> connexions) {
-        Connexions = connexions;
-        ComputePaths();
+        MapHeight = line;
+        DotsMap = DotsMap.SkipLast((nbLinesToFold + 1) * MapWidth).ToList();
+        Debug.Assert(DotsMap.Count == MapWidth * MapHeight);
     }
 
-    private void ComputePaths() {
-        foreach (var startConnexion in Connexions.Where(c => c.Cave1.IsStart)) {
-            var initialPath = new Path(new List<Cave>() { startConnexion.Cave1, startConnexion.Cave2 });
-            var branches = new List<Path>() { initialPath };
-            var brancheIndexesToExplore = new Queue<int>();
-            brancheIndexesToExplore.Enqueue(0);
-            while (brancheIndexesToExplore.Count > 0) {
-                var currentBranchIndex = brancheIndexesToExplore.Dequeue();
-                var currentBranch = branches[currentBranchIndex];
-                var currentCave = currentBranch.Caves.Last();
-                var connectedCaves = Connexions.Select(conn => conn.FindConnection(currentCave)).Where(cave => cave != null);
-                var nbConnectedCave = 0;
-                Cave? caveToAddToCurrentPath = null;
-                foreach (var connectedCave in connectedCaves) {
-                    var caveBeforeLastCave = currentBranch.GetBeforeLastCave();
-                    if (connectedCave == null || connectedCave.IsStart)
-                        continue; // do not go back to the start
-                    if (currentCave.IsBig && (caveBeforeLastCave?.IsBig ?? false) && (caveBeforeLastCave?.Name.Equals(connectedCave.Name) ?? false))
-                        continue; // avoid infinite back and forth between 2 big caves.
-                    if (!connectedCave.IsBig && currentBranch.Contains(connectedCave) && currentBranch.HasVisitedASmallCaveTwice)
-                        continue; // small cave already explored and we already explored 1 small cavern twice
-                    int indexToExplore;
-                    if (++nbConnectedCave == 1) {
-                        caveToAddToCurrentPath = connectedCave;
-                        indexToExplore = currentBranchIndex;
-                    } else {
-                        indexToExplore = branches.Count;
-                        branches.Add(new Path(currentBranch.Caves.Append(connectedCave)));
-                    }
-                    if (!connectedCave.IsEnd)
-                        brancheIndexesToExplore.Enqueue(indexToExplore);
-                }
-                if (caveToAddToCurrentPath != null)
-                    currentBranch.AddCave(caveToAddToCurrentPath);
+    public void FoldVertically(int column) {
+        var nbLinesToFold = MapWidth - column - 1;
+        for (int foldedColumn = 1; foldedColumn <= nbLinesToFold; foldedColumn++) {
+            for (int i = 0; i < MapHeight; i++) {
+                var currentIndex = column + foldedColumn + i * MapWidth;
+                var newIndex = column - foldedColumn + i * MapWidth;
+                DotsMap[newIndex] = DotsMap[newIndex] || DotsMap[currentIndex];
             }
-            Paths.AddRange(branches.Where(path => path.Caves.Last().IsEnd).ToList());
         }
-    }
-}
-class Path {
-    public List<Cave> Caves { get; private set; } = new List<Cave>();
-    public Path(IEnumerable<Cave> caves) {
-        foreach (var cave in caves) {
-            AddCave(cave);
+        var currentDotsMap = DotsMap.ToList();
+        DotsMap = new List<bool>();
+        for (int i = 0; i < MapWidth * MapHeight; i++) {
+            var x = i % MapWidth;
+            if (x < column) {
+                DotsMap.Add(currentDotsMap[i]);
+            }
         }
+        MapWidth = column;
+        Debug.Assert(DotsMap.Count == MapWidth * MapHeight);
     }
 
-    public bool HasVisitedASmallCaveTwice { get; private set; } = false;
-    public bool Contains(Cave cave) {
-        return Caves.Exists(c => c.Name.Equals(cave.Name, StringComparison.CurrentCulture));
-    }
-    public void AddCave(Cave cave) {
-        if (!cave.IsBig && Contains(cave)) {
-            HasVisitedASmallCaveTwice = true;
+    public void PrintToConsole() {
+        for (int i = 0; i < MapHeight; i++) {
+            Console.WriteLine(string.Concat(DotsMap.Skip(i * MapWidth).Take(MapWidth).Select(b => b ? '#' : '.')));
         }
-        Caves.Add(cave);
+        Console.WriteLine();
     }
-
-    public Cave? GetBeforeLastCave() {
-        return Caves.Count >= 2 ? Caves[Caves.Count - 2] : null;
-    }
-}
-
-class Connexion {
-    public Cave Cave1 { get; private set; }
-    public Cave Cave2 { get; private set; }
-    public Connexion(Cave cave1, Cave cave2) {
-        Cave1 = cave1.IsStart ? cave1 : cave2;
-        Cave2 = cave1.IsStart ? cave2 : cave1;
-    }
-    public Cave? FindConnection(Cave cave) {
-        return cave.Name.Equals(Cave1.Name, StringComparison.CurrentCulture) ? Cave2 : cave.Name.Equals(Cave2.Name, StringComparison.CurrentCulture) ? Cave1 : null;
-    }
-}
-
-class Cave {
-    public string Name { get; private set; }
-    public bool IsBig => char.IsUpper(Name[0]);
-    public bool IsEnd => "end".Equals(Name);
-    public bool IsStart => "start".Equals(Name);
-    public Cave(string name) => Name = name;
 }
