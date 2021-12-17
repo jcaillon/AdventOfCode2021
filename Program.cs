10151b98197fec7b9c79e403c8408cd970ad4136
Julien Caillon <julien.caillon@gmail.com>
2021-12-17 14:23:28 +0100

    day17-part2

--- Program.cs
+++ Program.cs
@@ -13,23 +13,23 @@ static class Puzzle {
         var targetArea = new Rectangle(reg[0], reg[2], reg[1] - reg[0], reg[3] - reg[2]);
 
         var oceanTrench = new OceanTrench();
-        oceanTrench.FindAllTrajectories(targetArea);
+        oceanTrench.FindAllSimulationsLandingTo(targetArea);
 
-        return $"The highest y position reached by a valid trajectory is {oceanTrench.Simulations.SelectMany(s => s.Trajectory).Select(p => p.Y).Max()}";
+        return $"There are a total of {oceanTrench.Simulations.Count()} initial velocity that satisfy the simulation goal";
     }
 }
 
 class OceanTrench {
     public List<Simulation> Simulations { get; private set; } = new List<Simulation>();
-    public void FindAllTrajectories(Rectangle targetArea) {
-        // initial x velocity must be non negative and < targetArea.Left because we can't overshoot target area after 1st step
-        // initial y velocity must be non negative (find highest curve) and velocity at y = 0 is equal to the opposite of the initial velocity
-        // so initial y velocity can't be more than targetArea.Top or next step after reaching y = 0 would overshoot
-        foreach (var initialVelocity in
-            from x in Enumerable.Range(1, targetArea.Left)
-            from y in Enumerable.Range(0, Math.Abs(targetArea.Top))
-            select new Point(x, y)) {
-            Simulations.Add(new Simulation(targetArea, new Point(0, 0), initialVelocity));
+    public void FindAllSimulationsLandingTo(Rectangle targetArea) {
+        var xVelMin = 1; // must be >0 to go somewhere
+        var xVelMax = targetArea.Right; // can't be more because we would overshoot target area after 1st step
+        var yVelMin = -Math.Abs(targetArea.Top); // can't be less because we would overshoot target area after 1st step
+        var yVelMax = Math.Abs(targetArea.Top); // at y = 0 velocity is equal to the opposite of the initial velocity so initial velocity can't be more than that or next step after reaching y = 0 would overshoot
+        for (int xVel = xVelMin; xVel <= xVelMax; xVel++) {
+            for (int yVel = yVelMin; yVel <= yVelMax; yVel++) {
+                Simulations.Add(new Simulation(targetArea, new Point(0, 0), new Point(xVel, yVel)));
+            }
         }
         Simulations = Simulations.Where(s => s.TrajectoryInTargetArea).ToList();
     }
@@ -53,10 +53,10 @@ class Simulation {
     private void Simulate() {
         var currentPoint = InitialPosition;
         var currentVelocity = InitialVelocity;
-        while(!TrajectoryInTargetArea && currentPoint.X <= TargetArea.Right && currentPoint.Y >= TargetArea.Bottom) {
+        while(!TrajectoryInTargetArea && currentPoint.X <= TargetArea.Right && currentPoint.Y >= TargetArea.Top) {
             currentPoint.Offset(currentVelocity.X, currentVelocity.Y);
             currentVelocity.Offset(currentVelocity.X > 0 ? -1 : 0, -1);
-            TrajectoryInTargetArea = TargetArea.Contains(currentPoint);
+            TrajectoryInTargetArea = currentPoint.X >= TargetArea.Left && currentPoint.X <= TargetArea.Right && currentPoint.Y >= TargetArea.Top && currentPoint.Y <= TargetArea.Bottom;
             Trajectory.Add(currentPoint);
         }
     }
